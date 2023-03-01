38d63d6b36d2c5a2d68c257e7de64e3515cfa811
lrhasak <lrhasak@stanford.edu>
2023-03-01 13:24:13 -0800

    first commit

--- supplemental/correlating_RC_results.m
+++ supplemental/correlating_RC_results.m
@@ -1,57 +1,53 @@
 %% Correlating A between conditions
-% TThis script takes RCA results and correlates the topographies (A).
+% This script takes RCA results and correlates the topographies (A).
+
+clear all; close all; clc
 %% find and load data
-  datalocation = '/Volumes/Seagate Backup Plus Drive/2020_Studies/2020_AV_RCA/2021_QP_redo/';
-    load(strcat(datalocation,'RCA/rcaResults_Freq_Condition 4.mat'))
-    alternating_A = rcaResult.A;
+datalocation_2019 = '/Users/lrhasak/code/Git/2022_AV_Analysis/2019_sessions_RCA/';
+datalocation_2022 = '/Users/lrhasak/code/Git/2022_AV_Analysis/';
+datalocation_combo = '/Users/lrhasak/code/Git/2022_AV_Analysis/combined_RCA/';
+
+% load 2019 data
+load(strcat(datalocation_2019, 'RCA/rcaResults_Freq_Alt Visual 1-back.mat'));
+c2019_A = rcaResult.A;
+
+% load 2022 data
+load(strcat(datalocation_2022,'2022_sessions_RCA/even/RCA/rcaResults_Freq_Condition_c2_nF2.mat'));
+c2022_A = rcaResult.A;
+
+% load combined data 
+load(strcat(datalocation_combo,'RCA/rcaResults_Freq_Combined_Alt_Vis_1-back.mat'));
+combo_A = rcaResult.A;
+
+% create component vectors
+    comp_1 = [c2019_A(:, 1), c2022_A(:, 1), combo_A(:, 1)];
+    comp_2 = [c2019_A(:, 2), c2022_A(:, 2), combo_A(:, 2)];
+    comp_3 = [c2019_A(:, 3), c2022_A(:, 3), combo_A(:, 3)];
+    comp_4 = [c2(:, 4), c7(:, 4), c10(:, 4)];
     
-  % attempt - consider this fake data
-dude = rc_1.A;
-    
-%     load(strcat(datalocation,'RCA/rcaResult_Freq_c10_flipped.mat'))
-%     incongruent_A = rcaResult.A;
-%     
-%     load(strcat(datalocation,'RCA/rcaResults_Freq_Condition 7.mat'))
-%     congruent_A = rcaResult.A;
-    
-   % load(strcat(datalocation,'RCA/rcaResult_Freq_rcaResult_c3_nf2_flipped.mat'))
-   % audio_A = rcaResult.A
- 
-    % 128 x 18: electrode x 6 components x 3 structs
-    A = [alternating_A, congruent_A, incongruent_A];
-    comp_1 = [alternating_A(:, 1), congruent_A(:, 1), incongruent_A(:, 1)];
-    comp_2 = [alternating_A(:, 2), congruent_A(:, 2), incongruent_A(:, 2)];
-    
-    A = [alternating_A, dude];
-    comp_1 = [alternating_A(:, 1), dude(:, 1)];
-    
-    % fake data shows a 98% correlation lol guess that makes sense though
+
+% plot correlations
+comps = {comp_1, comp_2, comp_3};
+titles = {'Condition 2 RC 1 Correlations', 'Condition 2 RC 2 Correlations', 'Condition 2 RC 3 Correlations'};
+
+for i = 1:numel(comps)
     figure;
-    [rho,pvalue,H] = corrplot(comp_1, 'varNames', {'old', 'new'})
-    
+    [rho,pvalue,H] = corrplot(comps{i}, 'varNames', {'2019', '2022', 'combo'}); % need to download Econometrics Toolbox
+    t = tril(rho, -1) > 0;
+    mirrors = find(t == 1);
+    for j=1:size(mirrors)
+        delete(subplot(3,3,mirrors(j)));    
+    end
+    title(titles{i});
+end
+
+
+
+%% PROGRESS
    
-%    alt_comp = zeros(128, 6);
-%     for i = 1:size(alternating_A, 2)
-%         for j = 1:length(alternating_A)
-%         alt_comp(i) = alternating_A(j)
-%         end
-%     end
+   % this actually does work
+   nComp = 6
+   for a = 1:nComp
+       comp{a} = [c2(:, nComp), c7(:, nComp), c10(:, nComp)];
+   end   
     
-    figure;
-    [rho,pvalue,H] = corrplot(comp_1, 'varNames', {'alt', 'con', 'incon'}) % need to download Econometrics Toolbox
-    title('Component 1 Correlations')
-  
-    figure; 
-    [rho,pvalue,H] = corrplot(comp_2, 'varNames', {'alt', 'con', 'incon'})
-    title('Component 2 Correlations')
-    
-% %  Find the index to remove the upper triangular part alone, this might
-% % be different for you base on you data but you can manually import the indexes
-% % or change the conditions in tril and find
-% t = tril(rho, -1) > 0;
-% mirrors = find(t == 1);
-%  
-% % delete the unnecessary subplots
-% for i=1:size(mirrors)
-%     delete(subplot(3,3,mirrors(i)));    
-% end
